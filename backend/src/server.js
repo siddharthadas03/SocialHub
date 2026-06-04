9bc3fe09ad420c16007c96a773504e3e0c47cf14
Siddhartha Das <siddharthadas620@gmail.com>
2026-06-04 12:35:21 +0530

    Initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,15 @@ const __dirname = path.dirname(__filename);
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: [
+      "https://social-hub-alpha-three.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
